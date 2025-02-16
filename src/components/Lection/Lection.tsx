4e4cfe74cd51567c26b0b437792162bdd02ed591
alvaradodaniel <alvaradodaniel128@gmail.com>
2025-02-16 13:41:29 -0600

    Add Lection component

--- src/components/Lection/Lection.tsx
+++ src/components/Lection/Lection.tsx
@@ -1,18 +1,27 @@
-import React, { FC, ReactNode } from "react";
+import React, { FC } from "react";
+import "./Lection.css";
 
 interface LectionProps {
-  children: ReactNode | ReactNode[];
+  title: string;
+  description: string;
+  imageSrc?: string;
 }
 
-const Lection: FC<LectionProps> = ({ children }) => {
+const Lection: FC<LectionProps> = ({
+  title,
+  description,
+  imageSrc = "https://cdn-icons-png.flaticon.com/512/1995/1995515.png",
+}) => {
   return (
-    <button
-      style={{
-        padding: "1em",
-      }}
-    >
-      {children}
-    </button>
+    <div className="lection-container">
+      <div>
+        <img src={imageSrc} alt="Lection image" className="lection-image"></img>
+      </div>
+      <div className="lection-texts">
+        <h1 className="lection-title">{title}</h1>
+        <p className="lection-paragraph">{description}</p>
+      </div>
+    </div>
   );
 };
 
